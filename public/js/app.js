9d6c96667ece675ea2147ab4d22f6600a1095460
Thapelo123 <Thapelomorethe95@gmail.com>
2019-04-15 16:21:28 +0200

    Remove unnessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('File is printed on javascript')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
